78e3f764b95f93d14650d00460fb27d513338427
fisty <emoessne@gmail.com>
2025-03-09 11:17:32 -0700

    Declaring x in multiple scopes. OK

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 
 // C++ Block Scope | Modern Cpp Series Ep. 12
 
-float x = 10.3f; // Global var. Globals are not good. try to avoid these
+//float x = 10.3f; // Global var. Globals are not good. try to avoid these
 
 int main() {
 
     {
-        x = 3.14f; // x only can be used in this scope. {}
+        int x = 3; // x only can be used in this scope. {}
     }
-    std::cout << "x: " << x << std::endl;
-    std::cout << "The size of x: " << sizeof(x) << std::endl;
+
+    {
+        int x = 5; // x only can be used in this scope. {}
+        std::cout << "x: " << x << std::endl;
+        std::cout << "The size of x: " << sizeof(x) << std::endl;
+    }
 
     return 0;
 }
