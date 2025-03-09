dfae8006222d0e4bd2a2218f99788348b54ba610
fisty <emoessne@gmail.com>
2025-03-09 10:59:11 -0700

    first char

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // C++ Primitive Data Types | Modern Cpp Series Ep. 10
 
 int main() {
-    double x = 3.143876438648f;
+    char x = 'a';
     std::cout << x << std::endl;
     std::cout << sizeof(x) << std::endl;
     return 0;
