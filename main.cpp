45190b5d0d79b148f274d540d78502ddcdd8dd72
fisty <emoessne@gmail.com>
2025-03-09 10:58:32 -0700

    first use of double

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // C++ Primitive Data Types | Modern Cpp Series Ep. 10
 
 int main() {
-    float x = 3.14f;
+    double x = 3.143876438648f;
     std::cout << x << std::endl;
     std::cout << sizeof(x) << std::endl;
     return 0;
