e3a39c3f8512b1c7ffbf4aa41a2f8e5e12b88a8d
fisty <emoessne@gmail.com>
2025-03-09 11:00:45 -0700

    using strings from the standard lib.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 
 // C++ Primitive Data Types | Modern Cpp Series Ep. 10
 
 int main() {
-    char x = 'a';
+    std::string x = "Hello World";
     std::cout << x << std::endl;
     std::cout << sizeof(x) << std::endl;
     return 0;
