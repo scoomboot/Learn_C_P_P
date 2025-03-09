616d980f117d0e027315a4bb47c21d3e932a27bc
fisty <emoessne@gmail.com>
2025-03-09 11:18:40 -0700

    Raw Arrays and std::array in C++ | Modern Cpp Series Ep. 13

--- main.cpp
+++ main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
 
-// C++ Block Scope | Modern Cpp Series Ep. 12
-
-//float x = 10.3f; // Global var. Globals are not good. try to avoid these
+// Raw Arrays and std::array in C++ | Modern Cpp Series Ep. 13
 
 int main() {
-
-    {
-        int x = 3; // x only can be used in this scope. {}
-    }
-
-    {
-        int x = 5; // x only can be used in this scope. {}
-        std::cout << "x: " << x << std::endl;
-        std::cout << "The size of x: " << sizeof(x) << std::endl;
-    }
 
     return 0;
 }
