038d7515c15e62c3fdf2faa10599a96f432806ad
fisty <emoessne@gmail.com>
2025-03-09 10:55:08 -0700

    first use of long long to store larger data.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // C++ Primitive Data Types | Modern Cpp Series Ep. 10
 
 int main() {
-    int x = 42;
+    long long x = 4222222212121221212;
     std::cout << x << std::endl;
     std::cout << sizeof(x) << std::endl;
     return 0;
